692c579beab2e4e60b6a7c01ba63dc5048d6af74
zohaib khan <zohaibkhan1162002@gmail>
2022-07-21 03:37:28 -0700

    make separte folder for Api

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import Suggestions from "./Suggestions.js";
+import { options, geoDB_URL } from "../API/fetchCities.js";
 import { BsGlobe } from "react-icons/bs";
 import { IoSearch } from "react-icons/io5";
 
@@ -8,23 +9,12 @@ const Header = () => {
   const [reply, setReply] = useState([]);
   const [focus, setFocus] = useState(false);
 
-  const options = {
-    method: "GET",
-    headers: {
-      "X-RapidAPI-Key": "c33169b7b7mshd4cdd31d9ac58dep1dfd47jsnf4695d416579",
-      "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
-    },
-  };
-
   const handleChange = (e) => {
     setValue(e.target.value);
     if (value < 3) {
       setReply([]);
     } else {
-      fetch(
-        `https://wft-geo-db.p.rapidapi.com/v1/geo/cities?namePrefix=${value}`,
-        options
-      )
+      fetch(`${geoDB_URL}${value}`, options)
         .then((response) => response.json())
         .then((response) => setReply(response.data))
         .catch((err) => {
