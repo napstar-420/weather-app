728995261b79e935e1dfb4bc2fa659e4ad615a72
zohaib khan <zohaibkhan1162002@gmail>
2022-08-28 12:23:25 -0700

    Create Header

---
+++ src/Components/Header.js
@@ -0,0 +1,21 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import React from 'react'
+import {BsInstagram} from 'react-icons/bs';
+import {HiOutlineMail} from 'react-icons/hi';
+import {FaLinkedinIn} from 'react-icons/fa';
+
+
+const Header = () => {
+  return (
+    <div className='flex justify-between py-4 px-10'>
+        <h3 className='text-white text-lg font-semibold z-10'>GEO WEATHER</h3>
+        <div className='flex items-center text-white z-10'>
+            <a href="#" target="_blank" rel="noopener noreferrer" className='p-2 mx-2 border border-white rounded-full hover:bg-white hover:text-black transition'><BsInstagram className='text-'/></a>
+            <a href="#" target="_blank" rel="noopener noreferrer"className='p-2 mx-2 border border-white rounded-full hover:bg-white hover:text-black transition'><HiOutlineMail className='text-'/></a>
+            <a href="#" target="_blank" rel="noopener noreferrer" className='p-2 mx-2 border border-white rounded-full hover:bg-white hover:text-black transition'><FaLinkedinIn className='text-'/></a>
+        </div>
+    </div>
+  )
+}
+
+export default Header
--- src/Components/home.js
+++ src/Components/home.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import Header from './Header.js'
+import web_bg from '../Images/web_bg.jpg'
 
 const Home = () => {
   return (
-    <div>
-        <h1 className='text-xl font-bold'>Home</h1>
+    <div className="w-screen h-screen after:w-[100%] after:h-[100%] after:bg-black after:content-['*'] after:absolute after:top-0 after:opacity-50" style={{backgroundImage: `url(${web_bg})`, backgroundSize: 'cover', backgroundRepeat: 'no-repeat'}}>
+      <Header className=""/>
     </div>
   )
 }
